b97ce3e77d4c339daae3e37b4686d6941996567f
ritzbitzn <ritzymail@gmail.com>
2014-01-02 23:45:19 -0800

    Added a "simple" mode that explains what is going on.

--- rpn-calc.py
+++ rpn-calc.py
@@ -13,8 +13,12 @@ try:
 	#Parsing command line arguments and splitting them into individual elements
 	argParser = ArgumentParser(description='Process a reverse-Polish notation equation.')
 	argParser.add_argument('eqn', metavar='item',  nargs='+', type=str, help='An item in a reverse-Polish notation equation.')
-	rpnEq = argParser.parse_args().eqn[0].replace('(', '').replace(')', '').replace('[', '').replace(']', '')\
+	argParser.add_argument('--simple', help='Print the stack at each step.')
+	endArgs =  argParser.parse_args()
+	rpnEq = endArgs.eqn[0].replace('(', '').replace(')', '').replace('[', '').replace(']', '')\
 	.replace('{', '').replace('}', '')
+
+	verbosity = endArgs.simple
 	eqn = split(rpnEq)
 	
 	#Converting all non-operator elements into floats.
@@ -25,29 +29,46 @@ try:
 	
 	#Reverse Polish notation algorithm
 	for i in range(len(eqn)):
+		if (verbosity):
+				print "Parsed element:" , eqn[i]
 		#Check if item is operator
 		if (str(eqn[i]) in ops):
+			
 			#Pop two variables to operate on
 			fItem = rpnStack.pop()
 			sItem = rpnStack.pop()
 			
 			#Perform corresponding operation and put back in stack
 			if (eqn[i] == '+'):
+				if (verbosity):
+					print "Adding", fItem, "and", sItem
 				rpnStack.append(fItem + sItem)
 			elif(eqn[i] == '-'):
+				if (verbosity):
+					print "Subtracting", fItem, "and", sItem
 				rpnStack.append(fItem - sItem)
 			elif (eqn[i] == '/'):
+				if (verbosity):
+					print "Dividing", sItem, "and", fItem
 				rpnStack.append(sItem / fItem)
 			elif (eqn[i] == '*'):
+				if (verbosity):
+					print "Multiplying", fItem, "and", sItem
 				rpnStack.append(fItem * sItem)
 			elif (eqn[i] == '^'):
-				rpnStack.append(fItem ** sItem)
+				if (verbosity):
+					print "Raising", sItem, "to the power of", fItem
+				rpnStack.append(sItem ** fItem)
 			else:
-				rpnStack.append(fItem % sItem)
+				if (verbosity):
+					print "Getting the remainder of", sItem, "divided by", fItem
+				rpnStack.append(sItem % fItem)
 				
 		else:
 			#Put number in stack
 			rpnStack.append(eqn[i])
+		if (verbosity):
+			print rpnStack
 
 	#Pop stack because the only element left should be the answer
 	print rpnStack.pop()
