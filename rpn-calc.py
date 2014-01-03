41ef09cfd23990a7369b94a7346f4ddc99bb4f48
ritzbitzn <ritzymail@gmail.com>
2014-01-02 18:33:51 -0800

    Got rid of the class system - it was unnecessarily inefficient.

--- rpn-calc.py
+++ rpn-calc.py
@@ -1,46 +1,10 @@
+#All of the imports
 from argparse import ArgumentParser
 from shlex import split
 from subprocess import call
-import sys
-		
-class rpnCalc():
-	def __init__(self):
-		self.rpnStack = []
-		self.ops = "*/+-%"
-		self.eqn = []
-	
-	def setEqn(self, rpnEq):
-		self.eqn = rpnEq
-		
-	def processEqn(self):
-		for i in range(len(self.eqn)):
-			if (self.eqn[i] in self.ops):
-				tempVar = 0
-				if (self.eqn[i] == '+'):
-					fItem = float(self.rpnStack.pop())
-					sItem = float(self.rpnStack.pop())
-					self.rpnStack.append(fItem + sItem)
-				elif(self.eqn[i] == '-'):
-					fItem = float(self.rpnStack.pop())
-					sItem = float(self.rpnStack.pop())
-					self.rpnStack.append(fItem - sItem)
-				elif (self.eqn[i] == '/'):
-					fItem = float(self.rpnStack.pop())
-					sItem = float(self.rpnStack.pop())
-					self.rpnStack.append(fItem / sItem)
-				elif (self.eqn[i] == '*'):
-					fItem = float(self.rpnStack.pop())
-					sItem = float(self.rpnStack.pop())
-					self.rpnStack.append(fItem * sItem)
-				else:
-					fItem = float(self.rpnStack.pop())
-					sItem = float(self.rpnStack.pop())
-					self.rpnStack.append(fItem % sItem)
-					
-			else:
-				self.rpnStack.append(self.eqn[i])
-		return self.rpnStack.pop()
-				
+
+rpnStack = []
+ops = "*/+-%"
 
 argParser = ArgumentParser(description='Process a reverse-Polish notation equation.')
 
@@ -48,9 +12,35 @@ argParser.add_argument('eqn', metavar='item',  nargs='+', type=str, help='An ite
 
 rpnEq = argParser.parse_args().eqn[0]
 
-rpnInst = rpnCalc()
-rpnInst.setEqn(split(rpnEq))
+eqn = split(rpnEq)
 
-x = rpnInst.processEqn()
 
-print x
+for i in range(len(eqn)):
+	if (eqn[i] in ops):
+		tempVar = 0
+		if (eqn[i] == '+'):
+			fItem = float(rpnStack.pop())
+			sItem = float(rpnStack.pop())
+			rpnStack.append(fItem + sItem)
+		elif(eqn[i] == '-'):
+			fItem = float(rpnStack.pop())
+			sItem = float(rpnStack.pop())
+			rpnStack.append(fItem - sItem)
+		elif (eqn[i] == '/'):
+			fItem = float(rpnStack.pop())
+			sItem = float(rpnStack.pop())
+			rpnStack.append(fItem / sItem)
+		elif (eqn[i] == '*'):
+			fItem = float(rpnStack.pop())
+			sItem = float(rpnStack.pop())
+			rpnStack.append(fItem * sItem)
+		else:
+			fItem = float(rpnStack.pop())
+			sItem = float(rpnStack.pop())
+			rpnStack.append(fItem % sItem)
+			
+	else:
+		rpnStack.append(eqn[i])
+
+print rpnStack.pop()
+
